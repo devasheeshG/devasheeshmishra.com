5de639432fc182641fd4e51c3daed552f7e4fe36
Vedant Sharma <svedant210@gmail.com>
2025-09-10 18:45:18 +0530

    final fix

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -75,7 +75,7 @@ export const RESUME_DATA = {
       location: "On-site",
       badges: ["AI", "Memory Systems", "Y Combinator"],
       title: "Founder and CEO",
-      logoUrl: "https://avatars.githubusercontent.com/u/193800346?v=4",
+      logoUrl: "/193800346.jpg",
       start: "2025",
       end: null,
       description: (
@@ -98,7 +98,7 @@ export const RESUME_DATA = {
       location: "On-site",
       badges: ["AI", "IoT", "Home Automation", "Startup"],
       title: "Founder and CEO",
-      logoUrl: "https://avatars.githubusercontent.com/u/99021598?v=4",
+      logoUrl: "/99021598.png",
       start: "2024",
       end: "2025",
       description: (
@@ -125,8 +125,7 @@ export const RESUME_DATA = {
       location: "On-site",
       badges: ["AI/ML", "Computer Vision", "Databases"],
       title: "AI/ML Engineer Intern",
-      logoUrl:
-        "https://proeffico.com/wp-content/uploads/2023/09/Copy-of-proeffico-logo-1.png",
+      logoUrl: "/Copy-of-proeffico-logo-1.png",
       start: "2024",
       end: "2024",
       description: (
@@ -148,7 +147,7 @@ export const RESUME_DATA = {
       location: "Remote",
       badges: ["Startup Accelerator", "Product Development"],
       title: "Builder, Nights & Weekends S5",
-      logoUrl: "https://avatars.githubusercontent.com/u/65048157?v=4",
+      logoUrl: "/65048157.png",
       start: "2024",
       end: "2024",
       description: (
