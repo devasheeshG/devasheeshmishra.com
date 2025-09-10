6aa6e11e0699d950496f77a992386385117eae25
Devasheesh Mishra <72092910+devasheeshG@users.noreply.github.com>
2025-09-10 19:34:24 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,21 +5,15 @@ module.exports = {
       {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
-        pathname: "/**",
       },
       {
         protocol: "https",
-        hostname: "cdn.worldvectorlogo.com",
-        pathname: "/**",
+        hostname: "media.geeksforgeeks.org",
       },
       {
         protocol: "https",
         hostname: "proeffico.com",
-        pathname: "/**",
       },
-      { protocol: "https", hostname: "www.gstatic.com", pathname: "/**" },
-      { protocol: "https", hostname: "www.srmist.edu.in", pathname: "/**" },
-      { protocol: "https", hostname: "media.licdn.com", pathname: "/**" },
     ],
   },
 };
