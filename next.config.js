85e534db980cd8d65c2d634b1e7f23ca07a93f9a
Vedant Sharma <svedant210@gmail.com>
2025-09-12 02:19:40 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -17,6 +17,11 @@ module.exports = {
         hostname: "proeffico.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "media.geeksforgeeks.org",
+        pathname: "/**",
+      },
       { protocol: "https", hostname: "www.gstatic.com", pathname: "/**" },
       { protocol: "https", hostname: "www.srmist.edu.in", pathname: "/**" },
       { protocol: "https", hostname: "media.licdn.com", pathname: "/**" },
