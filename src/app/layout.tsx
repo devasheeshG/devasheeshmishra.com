c35691bd77339fdafe76015ff726539b90cf19c3
Devasheesh Mishra <devasheeshmishra4@gmail.com>
2025-10-28 17:34:00 +0530

    Add Vercel Analytics and Speed Insights to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-// import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 
@@ -88,7 +89,8 @@ export default function RootLayout({
       <body>
         <ErrorBoundary>{children}</ErrorBoundary>
       </body>
-      {/* <Analytics /> */}
+      <SpeedInsights />
+      <Analytics />
     </html>
   );
 }
